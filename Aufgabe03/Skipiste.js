8373a0923988dda61b8565e3a3906ea8106dad86
Rebecca <Rebecca@rs-macbook-air.fu.hfu_guest.hs-furtwangen.de>
2017-11-02 21:34:22 +0100

    Aufgabe 3

--- Aufgabe03/Skipiste.js
+++ Aufgabe03/Skipiste.js
@@ -1,19 +1,19 @@
 /*
-Aufgabe: Aufgabe 2
+Aufgabe: Aufgabe 3
 Name: Rebecca Neß
 Matrikel: 256154
 Datum: 20.10.2017
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-var Aufgabe2;
-(function (Aufgabe2) {
+var Aufgabe3;
+(function (Aufgabe3) {
     window.addEventListener("load", canvasInput);
+    let canvas = document.getElementsByTagName("canvas")[0];
+    console.log(canvas);
+    let crc2 = canvas.getContext("2d");
+    console.log(crc2);
     function canvasInput() {
-        let canvas = document.getElementsByTagName("canvas")[0];
-        console.log(canvas);
-        let crc2 = canvas.getContext("2d");
-        console.log(crc2);
         // Hintergrund 
         crc2.fillStyle = "#6495ED";
         crc2.fillRect(0, 0, 800, 600);
@@ -22,7 +22,7 @@ var Aufgabe2;
         crc2.arc(80, 80, 40, 0, 2 * Math.PI);
         crc2.fillStyle = "#FFD700";
         crc2.fill();
-        // Berg One
+        /*/ Berg One
         crc2.beginPath();
         crc2.moveTo(0,700);
         crc2.lineTo(700,700);
@@ -51,7 +51,7 @@ var Aufgabe2;
         crc2.strokeStyle = "#BEBEBE";
         crc2.stroke();
         crc2.fillStyle = "#BEBEBE";
-        crc2.fill(); 
+        crc2.fill(); */
         // Skilift
         crc2.beginPath();
         crc2.moveTo(430, 130);
@@ -81,36 +81,30 @@ var Aufgabe2;
         crc2.fillStyle = "#ffffff";
         crc2.fill();
     }
-     
-        // Trees
-        for (var i = 1; i < 10; i++) {
-            drawTree(Math.floor(Math.random() * (400 - 60 + 1) + 200), Math.floor(Math.random() * (600 - 300 + 1) + 400), crc2);
-        }
-        // Schneeflocken
-        for (var i = 1; i < 100; i++) {
-            drawSnowflake(Math.floor(Math.random() * (800 + 1)), Math.floor(Math.random() * (600 + 1)), crc2);
-        }
-       
-      
-        function drawTree(x, y, crc2) {
-            crc2.beginPath();
-            crc2.moveTo(x - 15, y);
-            crc2.lineTo(x + 27, y);
-            crc2.lineTo(x + 5, y - 60);
-            crc2.lineTo(x - 15, y);
-            crc2.fillStyle = "#228B22";
-            crc2.fill();
-            crc2.closePath();
-        }
-        
-        function drawSnowflake(x, y, crc2) {
-            crc2.beginPath();
-            crc2.arc(x, y, 4, 0 * Math.PI, 2.0 * Math.PI);
-            crc2.fillStyle = "#fff";
-            crc2.fill();
-            crc2.closePath();
-        }
-        
-    
-})(Aufgabe2 || (Aufgabe2 = {}));
+    // Trees
+    for (var i = 1; i < 11; i++) {
+        drawTree(Math.floor(Math.random() * (340 - 15 + 1) + 15), Math.floor(Math.random() * (570 - 250 + 1) + 250), crc2);
+    }
+    // Schneeflocken
+    for (var i = 1; i < 40; i++) {
+        drawSnowflake(Math.floor(Math.random() * (800 + 1)), Math.floor(Math.random() * (600 + 1)), crc2);
+    }
+    function drawTree(_x, _y, crc2) {
+        crc2.beginPath();
+        crc2.moveTo(_x - 14, _y);
+        crc2.lineTo(_x + 26, _y);
+        crc2.lineTo(_x + 4, _y - 60);
+        crc2.lineTo(_x - 14, _y);
+        crc2.fillStyle = "#228B22";
+        crc2.fill();
+        crc2.closePath();
+    }
+    function drawSnowflake(_x, _y, crc2) {
+        crc2.beginPath();
+        crc2.arc(_x, _y, 4, 0 * Math.PI, 2.0 * Math.PI);
+        crc2.fillStyle = "#111";
+        crc2.fill();
+        crc2.closePath();
+    }
+})(Aufgabe3 || (Aufgabe3 = {}));
 //# sourceMappingURL=Skipiste.js.map
--- Aufgabe03/Skipiste.ts
+++ Aufgabe03/Skipiste.ts
@@ -1,20 +1,19 @@
 /*  
-Aufgabe: Aufgabe 2 
+Aufgabe: Aufgabe 3
 Name: Rebecca Neß
 Matrikel: 256154
 Datum: 20.10.2017
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-namespace Aufgabe2 {
+namespace Aufgabe3 {
     
 window.addEventListener("load", canvasInput);
     
- let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas") [0];
-  console.log(canvas);
-    
+let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas") [0];
+   console.log(canvas);
 let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
- console.log(crc2);
+   console.log(crc2);
 
 function canvasInput(): void {
     // Hintergrund 
@@ -25,7 +24,7 @@ function canvasInput(): void {
     crc2.arc(80,80,40,0,2 * Math.PI)
     crc2.fillStyle ="#FFD700";
     crc2.fill();
-    // Berg One
+    /*/ Berg One
     crc2.beginPath();
     crc2.moveTo(0,700);
     crc2.lineTo(700,700);
@@ -54,7 +53,7 @@ function canvasInput(): void {
     crc2.strokeStyle = "#BEBEBE";
     crc2.stroke();
     crc2.fillStyle = "#BEBEBE";
-    crc2.fill(); 
+    crc2.fill(); */
     // Skilift
     crc2.beginPath();
     crc2.moveTo(430, 130);
@@ -83,12 +82,13 @@ function canvasInput(): void {
     crc2.arc(705,95,20,2,4 * Math.PI);
     crc2.fillStyle ="#ffffff";
     crc2.fill();
-     }
+    }
     
     // Trees
     for (var i:number = 1; i < 11; i++) {
         drawTree(Math.floor(Math.random() * (340 - 15 + 1) + 15), Math.floor(Math.random() * (570 - 250 + 1) + 250), crc2);
     }
+    
     // Schneeflocken
     for (var i:number = 1; i < 40; i++) {
         drawSnowflake(Math.floor(Math.random() * (800 + 1)), Math.floor(Math.random() * (600 + 1)), crc2);
@@ -107,9 +107,7 @@ function canvasInput(): void {
     
     function drawSnowflake(_x: number, _y: number, crc2: any) {
         crc2.beginPath();
-        crc2.arc(x, y, 4, 0 * Math.PI, 2.0 * Math.PI);
-        //crc2.strokeStyle = "#BDBDBD";
-        //crc2.stroke();
+        crc2.arc(_x, _y, 4, 0 * Math.PI, 2.0 * Math.PI);
         crc2.fillStyle = "#111";
         crc2.fill();
         crc2.closePath();
